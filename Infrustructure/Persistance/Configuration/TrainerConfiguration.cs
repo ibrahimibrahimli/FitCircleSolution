55c987afabb02dbd7f54483fb1b98ac473347d31
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-05-19 22:15:51 +0200

    Add Trainer and TrainerRating configuration classes
    Implemented IEntityTypeConfiguration for Trainer and TrainerRating entities. Defined table names, primary keys, and property constraints, including required fields and maximum lengths. Established a relationship between TrainerRating and Trainer with cascading delete behavior.

---
+++ Infrustructure/Persistance/Configuration/TrainerConfiguration.cs
@@ -0,0 +1,42 @@
+﻿using Domain.Entities;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Metadata.Builders;
+
+namespace Persistance.Configuration
+{
+    public class TrainerConfiguration : IEntityTypeConfiguration<Trainer>
+    {
+        public void Configure(EntityTypeBuilder<Trainer> builder)
+        {
+            builder.ToTable("Trainers");
+
+            builder.HasKey(t => t.Id);
+
+            builder.Property(t => t.Firstname)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(t => t.Lastname)
+                .IsRequired()
+                .HasMaxLength(50);
+
+            builder.Property(t => t.Email)
+                .IsRequired()
+                .HasMaxLength(100);
+
+            builder.Property(t => t.PhoneNumber)
+                .IsRequired()
+                .HasMaxLength(20);
+
+            builder.Property(t => t.Bio)
+                .HasMaxLength(1000);
+
+            builder.Property(t => t.ProfileImageUrl)
+                .HasMaxLength(250);
+
+            builder.Property(t => t.InstagramHandle)
+                .HasMaxLength(100);
+
+        }
+    }
+}
