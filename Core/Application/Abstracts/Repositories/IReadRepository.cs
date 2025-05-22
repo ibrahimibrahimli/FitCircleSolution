dc168322cd0925b735ab3abbca41a7c6704cba1b
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-05-22 23:09:35 +0200

    Add generic repository interfaces and implementations
    Introduced `IReadRepository<T>` and `IWriteRepository<T>` interfaces for managing entities derived from `BaseAuditableEntity`. Implemented `GenericReadRepository<T>` for reading operations with `FitCircleDbContext`. Created `GenericWriteRepository<T>` with method stubs for writing operations, pending implementation. These changes establish a generic repository pattern to enhance data access and promote code reusability.

---
+++ Core/Application/Abstracts/Repositories/IReadRepository.cs
@@ -0,0 +1,11 @@
+﻿using Domain.Common;
+
+namespace Application.Abstracts.Repositories
+{
+    public interface IReadRepository<T> where T : BaseAuditableEntity
+    {
+        Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default);
+        Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default);
+        IQueryable<T> Query();
+    }
+}
---
+++ Infrustructure/Persistance/Repositories/GenericReadRepository.cs
@@ -0,0 +1,37 @@
+﻿using Application.Abstracts.Repositories;
+using Domain.Common;
+using Microsoft.EntityFrameworkCore;
+using Persistance.Context;
+
+namespace Persistance.Repositories
+{
+    public class GenericReadRepository<T> : IReadRepository<T> where T : BaseAuditableEntity
+    {
+        readonly FitCircleDbContext _context;
+
+        public GenericReadRepository(FitCircleDbContext context)
+        {
+            _context = context;
+        }
+
+        public async Task<IReadOnlyList<T>> GetAllAsync(CancellationToken cancellationToken = default)
+        {
+            return await _context.Set<T>().ToListAsync(cancellationToken);
+        }
+
+        public async Task<T> GetByIdAsync(Guid id, CancellationToken cancellationToken = default)
+        {
+            var entity = await _context.Set<T>().FindAsync([id], cancellationToken).ConfigureAwait(false);
+            if (entity == null)
+            {
+                throw new InvalidOperationException($"Entity of type {typeof(T).Name} with ID {id} was not found.");
+            }
+            return entity;
+        }
+
+        public IQueryable<T> Query()
+        {
+            return _context.Set<T>().AsQueryable();
+        }
+    }
+}
