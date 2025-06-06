cfd702efe35a920ae8425c2984feb45044ae5956
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-06-06 23:41:43 +0200

    Add MediatR support and city creation features
    Updated Application.csproj to include MediatR packages and added folder structure for city features. Introduced CreateCityCommandHandler to handle city creation requests using ICityWriteRepository. Added CreateCityCommandRequest for encapsulating city creation data and created CreateCityDto to define necessary properties for city creation.

---
+++ Core/Application/Features/Cities/Dtos/CreateCityDto.cs
@@ -0,0 +1,9 @@
+﻿namespace Application.Features.Cities.Dtos
+{
+    public class CreateCityDto
+    {
+        public string Name { get; set; } = default!;
+        public string PostalCode { get; set; } = default!;
+        public Guid CountryId { get; set; }
+    }
+}
