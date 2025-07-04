739d47a36267d950fbc62dab35588520d2cf7d17
ibrahimibrahimli <dev.ibrahimlii@gmail.com>
2025-07-04 22:05:26 +0200

    Enhance city management features and security
    - Removed folder reference for `Features\Cities\Queries\` in `Application.csproj`.
    - Added `Authorize` attribute to `Create` method in `CityController.cs` to restrict access to "Admin" users.
    - Introduced `CityDto` class in `CityDto.cs` to represent city data.
    - Created `GetAllCitiesQueryHandler` in `GetAllCitiesQueryHandler.cs` for retrieving all cities.
    - Added `GetAllCitiesQueryRequest` class in `GetAllCitiesQueryRequest.cs` to facilitate city retrieval requests.

---
+++ Core/Application/Features/Cities/Queries/GetAllCitiesQueryRequest.cs
@@ -0,0 +1,9 @@
+﻿using Application.Features.Cities.Dtos;
+using MediatR;
+
+namespace Application.Features.Cities.Queries
+{
+    public class GetAllCitiesQueryRequest : IRequest<List<CityDto>>
+    {
+    }
+}
